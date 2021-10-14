fd746b24a627263b5e6930ed3a1cee893b3625bf
ThankiAshish <thankiashish555@gmail.com>
2021-10-14 16:29:27 +0530

    Applied Styling

--- client/src/screens/register.jsx
+++ client/src/screens/register.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import authSvg from "../assets/auth.svg";
 import { ToastContainer, toast } from "react-toastify";
 import { isAuth } from "../helpers/auth";
 import axios from "axios";
@@ -8,119 +7,106 @@ import "../styles/Components.css";
 import "../styles/queries.css";
 
 const Register = () => {
-    const [formData, setFormData] = useState({
-        email: "",
-        passwordInput: "",
-        confirmPassword: ""
-    });
+  const [formData, setFormData] = useState({
+    email: "",
+    passwordInput: "",
+    confirmPassword: "",
+  });
 
-    const { email, passwordInput, confirmPassword } = formData;
+  const { email, passwordInput, confirmPassword } = formData;
 
-    const handleChange = text => e => {
-        setFormData({...formData, [text]: e.target.value});
+  const handleChange = (text) => (e) => {
+    setFormData({ ...formData, [text]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let id;
+    if (email && passwordInput) {
+      if (passwordInput === confirmPassword) {
+        id = toast.info("Please Wait...", { autoClose: false });
+        axios
+          .post(`${process.env.REACT_APP_API_URL}/register`, {
+            email,
+            password: passwordInput,
+          })
+          .then((res) => {
+            setFormData({
+              ...formData,
+              email: "",
+              passwordInput: "",
+              confirmPassword: "",
+            });
+            toast.update(id, {
+              render: res.data.message,
+              type: "success",
+              isLoading: false,
+            });
+          })
+          .catch((err) => {
+            toast.update(id, {
+              render: err.response.data.errors,
+              type: "error",
+              isLoading: false,
+            });
+          });
+      } else {
+        toast.error("Passwords don't match");
+      }
+    } else {
+      toast.error("Please fill all the fields");
     }
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        let id;
-        if(email && passwordInput) {
-            if(passwordInput === confirmPassword) {
-                id = toast.info("Please Wait...", { autoClose: false });
-                axios
-                    .post(`${process.env.REACT_APP_API_URL}/register`, {
-                        email, password: passwordInput
-                    })
-                    .then(res => {
-                        setFormData({
-                            ...formData,
-                            email: "",
-                            passwordInput: "",
-                            confirmPassword: "" 
-                        })
-                        toast.update(id, { render: res.data.message, type: "success", isLoading: false });
-                        // toast.success(res.data.message);
-                    })
-                    .catch(err => {
-                        toast.update(id, { render: err.response.data.errors, type: "error", isLoading: false });
-                        // toast.error(err.response.data.errors);
-                    })
-            } else {
-                toast.error("Passwords don't match");
-            }
-        } else {
-            toast.error("Please fill all the fields");
-        }
-    }
-
-    return (
-        <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-            {isAuth() ? <Redirect to="/" /> : null }
-            <ToastContainer />
-            <div className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
-                <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-                    <div className="mt-12 flex flex-col items-center">
-                        <h1 className="text-2xl xl:text-3xl font-extrabold">
-                            Sign Up
-                        </h1>
-                        <form 
-                            className="w-full flex-1 mt-8 text-indigo-500"
-                            onSubmit={handleSubmit}
-                        >
-                            <div className="mx-auto max-w-xs relative">
-                                <input 
-                                    type="email" 
-                                    placeholder="Email"
-                                    onChange={handleChange("email")}
-                                    value={email}
-                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                                />
-                                <input 
-                                    type="password" 
-                                    placeholder="Password"
-                                    onChange={handleChange("passwordInput")}
-                                    value={passwordInput}
-                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                                />
-                                <input 
-                                    type="password" 
-                                    placeholder="Confirm Password"
-                                    onChange={handleChange("confirmPassword")}
-                                    value={confirmPassword}
-                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                                />
-                                <button 
-                                    type="submit"
-                                    className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                                >
-                                    Sign Up
-                                </button>
-                            </div>
-                            <div className="my-12 border-b text-center">
-                                <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                                    Or Sign with Email or Social Login
-                                </div>
-                            </div>
-                            <div className="flex flex-col items-center">
-                                <a 
-                                    className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5"
-                                    href="/login"
-                                    target="_self"
-                                >
-                                    Sign In
-                                </a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div className='flex-1 bg-indigo-100 text-center hidden lg:flex'>
-                    <div
-                        className='m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat'
-                        style={{ backgroundImage: `url(${authSvg})` }}
-                    ></div>
-                </div>
+  return (
+    <div className="form-wrapper">
+      {isAuth() ? <Redirect to="/" /> : null}
+      <ToastContainer />
+      <div className="illustration sign-up"></div>
+      <div className="login-wrapper">
+        <form onSubmit={handleSubmit}>
+          <div className="inner-form-wrapper">
+            <h1>Sign Up</h1>
+            <a href="/login">
+              Already have an account? <span className="accent">Login</span>
+            </a>
+            <div className="field-wrapper">
+              <label>Email *</label>
+              <input
+                type="email"
+                placeholder="Email"
+                onChange={handleChange("email")}
+                value={email}
+              />
             </div>
-        </div>
-    );
+            <div className="field-wrapper">
+              <label>Password *</label>
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={handleChange("passwordInput")}
+                value={passwordInput}
+              />
+            </div>
+            <div className="field-wrapper">
+              <label>Confirm Password *</label>
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                onChange={handleChange("confirmPassword")}
+                value={confirmPassword}
+              />
+            </div>
+            <div className="btn-wrapper">
+              <button className="btn" type="submit">
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
